3c9061955af0fab91e88578524e28af4e0b9ee1d
muuo <anthonymuuo92@gmail.com>
2025-06-11 06:26:51 +0300

    route parameters like id.. and using the Param() from nest common

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'this returns all coffees available';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this is is the id ${id} and its dynamic`;
   }
 }
