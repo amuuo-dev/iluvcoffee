47c5388408b253be74c179239051a43ceaac40f5
muuo <anthonymuuo92@gmail.com>
2025-06-14 08:04:22 +0300

    using cascading and creating a flavor repo for creating flavors and adding existing ones

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -9,12 +9,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Flavor } from './enitities/flavor.entities';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly CoffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly FlavorRepository: Repository<Flavor>,
   ) {}
 
   async findAll() {
@@ -38,14 +41,26 @@ export class CoffeesService {
   }
 
   async create(createCoffeeDto: CreateCoffeeDto) {
-    const coffee = this.CoffeeRepository.create(createCoffeeDto);
+    const flavors = await Promise.all(
+      createCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+    );
+    const coffee = this.CoffeeRepository.create({
+      ...createCoffeeDto,
+      flavors,
+    });
     return await this.CoffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const flavors =
+      updateCoffeeDto.flavors &&
+      (await Promise.all(
+        updateCoffeeDto.flavors?.map((name) => this.preloadFlavorByName(name)),
+      ));
     const coffee = await this.CoffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavors,
     });
     if (!coffee) {
       throw new NotFoundException(`coffee #${id} not found`);
@@ -56,5 +71,15 @@ export class CoffeesService {
   async remove(id: string) {
     const coffee = await this.findOne(id);
     return this.CoffeeRepository.remove(coffee);
+  }
+  //like creating a flavor from flavor repo
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.FlavorRepository.findOne({
+      where: { name },
+    });
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.FlavorRepository.create({ name });
   }
 }
--- src/coffees/enitities/coffee.entities.ts
+++ src/coffees/enitities/coffee.entities.ts
@@ -19,6 +19,6 @@ export class Coffee {
   brand: string;
 
   @JoinTable()
-  @ManyToMany(() => Flavor, (flavor) => flavor.coffees)
+  @ManyToMany(() => Flavor, (flavor) => flavor.coffees, { cascade: true })
   flavors: Flavor[];
 }
