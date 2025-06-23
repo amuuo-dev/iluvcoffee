8f2827dab12a60460894779291ae950755b24a7d
muuo <anthonymuuo92@gmail.com>
2025-06-23 06:48:04 +0300

    leverage async providers in useFactory this helps while fetching or doing somethings that require some time like fetching from the database

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,26 +6,21 @@ import { Coffee } from './enitities/coffee.entities';
 import { Flavor } from './enitities/flavor.entities';
 import { Event } from 'src/events/entities/event.entity/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
-
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    // do something..
-    return ['nescafe', 'chocolate', 'buddy brew'];
-  }
-}
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (datasource: DataSource): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
+      inject: [DataSource],
     },
   ],
   exports: [CoffeesService],
