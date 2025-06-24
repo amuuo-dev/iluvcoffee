7bd86445b2c6c24f6bd1c756fb3d0d7f3254eaa4
muuo <anthonymuuo92@gmail.com>
2025-06-24 06:49:14 +0300

    understood the scope that is passed in the injectable the singeton, which is the default the request which calls everytime the request is passed and the lastly the TRANSIENT which created new instance in  every injection

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,6 +4,7 @@ import {
   Inject,
   Injectable,
   NotFoundException,
+  Scope,
 } from '@nestjs/common';
 import { Coffee } from './enitities/coffee.entities';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,7 +16,7 @@ import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/paginati
 import { Event } from 'src/events/entities/event.entity/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
@@ -25,7 +26,7 @@ export class CoffeesService {
     private readonly datasource: DataSource,
     @Inject(COFFEE_BRANDS) coffeeBrands: string[],
   ) {
-    console.log(coffeeBrands);
+    console.log('coffeesService instantiated');
   }
 
   async findAll(paginationQuery: PaginationQueryDto) {
