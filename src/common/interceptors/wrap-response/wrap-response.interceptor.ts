f529e1b883a8082fc60341dc5b8bae8feb10059e
muuo <anthonymuuo92@gmail.com>
2025-08-01 07:08:15 +0300

    added pointcuts with interceptors,, this helped me manipulate the response by wrapping it in data obj

---
+++ src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before the interceptor....');
+    return next.handle().pipe(map((data) => ({ data })));
+  }
+}
