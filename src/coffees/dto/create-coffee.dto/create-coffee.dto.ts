6dc7824bf547aad3d0a2bf104aad96b712096e9a
muuo <anthonymuuo92@gmail.com>
2025-08-02 06:04:23 +0300

    adding example responses to describe schema in the create-coffee-dto also returned to the pagination to required was a bad idea to fetch all

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,9 +1,16 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'This is the coffee name' })
   @IsString()
   readonly name: string;
+
+  @ApiProperty({ description: 'This is the coffee brand field' })
   @IsString()
   readonly brand: string;
+
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
-  readonly flavors: [];
+  readonly flavors: string[];
 }
